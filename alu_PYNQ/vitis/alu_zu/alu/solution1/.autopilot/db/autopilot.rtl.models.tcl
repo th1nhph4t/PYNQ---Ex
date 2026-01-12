8b27f521330a1458a43de7cb611aec24f666a5a5
Thinh Phat <phat862003@gmail.com>
2026-01-12 15:36:13 +0700

    HLS

--- alu_PYNQ/vitis/alu_zu/alu/solution1/.autopilot/db/autopilot.rtl.models.tcl
+++
@@ -1,7 +0,0 @@
-set SynModuleInfo {
-  {SRCNAME ALU MODELNAME ALU RTLNAME ALU IS_TOP 1
-    SUBMODULES {
-      {MODELNAME ALU_control_s_axi RTLNAME ALU_control_s_axi BINDTYPE interface TYPE interface_s_axilite}
-    }
-  }
-}
--- alu_PYNQ/vitis/alu_zu/alu/solution1/.autopilot/db/kernel_module_hierarchy.tcl
+++
@@ -1,3 +0,0 @@
-set ModuleHierarchy {[{
-"Name" : "ALU","ID" : "0","Type" : "sequential"
-}]}
