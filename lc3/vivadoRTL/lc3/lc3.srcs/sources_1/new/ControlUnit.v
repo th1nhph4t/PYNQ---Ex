5c119a42b3f693f178457ee45840d2c7111a805f
Thinh Phat <phat862003@gmail.com>
2026-01-12 15:32:39 +0700

    PYNQ EX

--- lc3/vivadoRTL/lc3/lc3.srcs/sources_1/new/ControlUnit.v
+++
@@ -1,7 +0,0 @@
-
-module ControlUnit(
-    input  [15:0] IR,
-    output [3:0]  Opcode
-);
-    assign Opcode = IR[15:12];
-endmodule
--- module/hls/analog/analog/solution1/directives.tcl
+++
@@ -1,7 +0,0 @@
-############################################################
-## This file is generated automatically by Vitis HLS.
-## Please DO NOT edit it.
-## Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
-## Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
-############################################################
-set_directive_top -name analog_open_xadc "analog_open_xadc"
